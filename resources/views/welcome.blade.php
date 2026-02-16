e98aab9fc7fb79bf5b05fe8461e86a16adeade84
gilbeee-web <125279875+gilbeee-web@users.noreply.github.com>
2026-02-16 21:16:04 +0800

    Initial Laraavel Project Setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Senior PWD System</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <h1 class="text-3xl text-white font-bold underline">Hello world!</h1>
+    </body>
+</html>
